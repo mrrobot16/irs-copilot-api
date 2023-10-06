8b11a4dce79e2ef7a21e14cf06f857f01c70e1cf
Hector G <hmgarcia06@gmail.com>
2023-10-06 17:55:44 -0400

    Added config.py file to add all .env variables to be reused within the app

---
+++ packages/python/api-v0/config.py
@@ -0,0 +1,12 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+APP_ENV = os.environ.get('APP_ENV')
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+OPENAI_GPT4_API_KEY = os.getenv("OPENAI_GPT4_API_KEY")
