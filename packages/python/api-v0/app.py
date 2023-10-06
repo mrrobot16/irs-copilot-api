e56c3f050c3ac9a1e409f05a4b53c10721bb3634
Hector G <hmgarcia06@gmail.com>
2023-10-06 17:59:14 -0400

    Simplified and refactored app.py folder where we implemented new routes

--- packages/python/api-v0/app.py
+++ packages/python/api-v0/app.py
@@ -1,41 +1,28 @@
 import json
 from flask import Flask, jsonify, request
 import openai
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
-openai_api_key = os.getenv("open_ai_api_key")
+from routes.gpt import gpt
+from routes.firebase import firebase
 
-openai.api_key = openai_api_key
+def create_app():
 
-app = Flask(__name__)
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    prompt = request.get_json()['prompt']
-    response = gpt3_completion(prompt)
-    return jsonify({'response': response})
+    openai_api_key = app.config['OPENAI_API_KEY']
+    openai.api_key = openai_api_key
 
-def gpt3_completion(prompt, engine='gpt-3.5-turbo-16k', temp=0.5, top_p=0.3, tokens=1000):
-    prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
-    try:
-        response = openai.ChatCompletion.create(
-            model=engine,
-            messages=[
-                {"role": "system", "content": "You are a helpful tax CPA for the United States of America with experience working with the IRS."},
-                {"role": "user", "content": prompt}
-            ],
-            temperature=temp,
-            max_tokens=tokens
-        )
-        return response['choices'][0]['message']['content']
-    except Exception as oops:
+    app.register_blueprint(gpt, url_prefix='/gpt')
+    app.register_blueprint(firebase, url_prefix='/firebase')
 
-        return "GPT-3 error: %s" % oops
-    
- 
+    @app.route('/')
+    def health():
+        return jsonify({'status': 'ok'})
 
+    return app
 
-if __name__ == '__main__':
-   app.run(port=5000)
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
