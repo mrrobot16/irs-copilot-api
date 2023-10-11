579d443fa8e00f7adf767a98f44eec2750acb78b
Hector G <hmgarcia06@gmail.com>
2023-10-11 19:07:05 -0400

    Added test for deactivate/activate user

--- tests/models.test.py
+++ tests/models.test.py
@@ -3,7 +3,7 @@ from firebase import firestore_db, firestore
 
 def create_user():
     user = User(firestore_db)
-    return user.create()
+    return user.new()
 
 def create_conversation(user_id):
     conversation = Conversation(firestore_db)
@@ -17,14 +17,22 @@ def update_user(id, email):
     user = User(firestore_db)
     return user.update(id, email)
 
+def activate_user(id):
+    user = User(firestore_db)
+    return user.activate(id)
+
+def deactivate_user(id):
+    user = User(firestore_db)
+    return user.deactivate(id)
+
 def delete_user(id):
     user = User(firestore_db)
     return user.delete(id)
 
 # Create User
 user = create_user()
-# # user_id = user.get().to_dict().get('id')
-user_id = user
+# user_id = user.get().to_dict().get('id')
+user_id = user['id']
 print('user_id', user_id)
 
 conversation = create_conversation(user_id)
@@ -42,13 +50,22 @@ print('message_id', message_id)
 
 # NOTE: Below are harded coded existing user id for testing update and delete methods.
 # existing_user_id = 'b51ccb22-ed74-4bd2-8'
+existing_user_id = '79047852-cdf3-47d4-b'
 # existing_user_id = ''
-# print('existing_user_id', existing_user_id)
+print('existing_user_id', existing_user_id)
 
 # Update User
-# email = 'hector@gmail.com'
-# user = update_user(existing_user_id, email)
-# print('updated_user', user)
+email = 'hector@gmail.com'
+user = update_user(existing_user_id, email)
+print('updated_user', user)
+
+# Deactivate User
+deactivated_user = deactivate_user(existing_user_id)
+print('deactivated_user', deactivated_user)
+
+# Activate User
+activated_user = activate_user(existing_user_id)
+print('activated_user', activated_user)
 
 # Delete User
 # deleted_user = delete_user(existing_user_id)
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+import bcrypt
 
 # Generate a unique ID
 def generate_unique_id(size=20):
@@ -14,6 +15,17 @@ def methods_and_attributes(obj):
     for item in dir(obj):
         if callable(getattr(obj, item)):
             print(item)
+
+# Generate a salt & Hash the password along with the salt
+def hash_password(plain_text_password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(plain_text_password.encode('utf-8'), salt)
+    return hashed_password
+
+# Check the provided password against the hashed one
+def verify_hashed_password(plain_text_password, hashed_password):
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
+
 
 # methods_and_attributes = dir(user)
 
