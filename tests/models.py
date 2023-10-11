9d8e7f0aa442800da702f8767773e63bd17b7702
Hector G <hmgarcia06@gmail.com>
2023-10-11 16:52:01 -0400

    Added tests for user update method

--- tests/models.py
+++ tests/models.py
@@ -9,14 +9,15 @@ class User:
         self.db = db
         self.collection_ref = self.db.collection('users')
 
-    def create(self, conversations = []):
+    def create(self, email = 'black_email@blank.com', conversations = []):
         self.user_id = generate_unique_id()  # Generate a unique 20-character ID
         self.created_at = generate_timestamp()  # Get current timestamp
         self.conversations = conversations  # Initialize with an empty array of conversation references
-        
+        self.email = email
         user = {
             'id': self.user_id,
             'created_at': self.created_at,
+            'email': self.email,
             'conversations': self.conversations
         }
         
@@ -24,7 +25,20 @@ class User:
         user_ref.set(user)
         # return user_ref
         return self.user_id
-
+    
+    def update(self, id, email):
+        self.id = id
+        self.email = email
+        self.updated_at = generate_timestamp()
+        user = {
+            'id': self.id,
+            'email': self.email,
+            'updated_at': self.updated_at
+        }
+        # Here, you can use the set method with merge=True to update or create if the document doesn't exist
+        users_ref = self.collection_ref.document(self.id)
+        users_ref.set(user, merge=True)
+        return user
 class Conversation:
 
     def __init__(self, db):
--- tests/models.test.py
+++ tests/models.test.py
@@ -13,22 +13,31 @@ def create_message(user_id, conversation_id, content, role):
     message = Message(firestore_db, user_id, conversation_id)
     return message.create(user_id, conversation_id, content, role)
 
-def create_user_conversation_message():
-    user = create_user()
-    # user_id = user.get().to_dict().get('id')
-    user_id = user
-    print('user_id', user_id)
+def update_user(id, email):
+    user = User(firestore_db)
+    return user.update(id, email)
 
-    conversation = create_conversation(user_id)
-    # conversation_id = conversation.get().to_dict().get('id')
-    conversation_id = conversation
-    print('conversation_id', conversation_id)
+# user = create_user()
+# # user_id = user.get().to_dict().get('id')
+# user_id = user
+# print('user_id', user_id)
 
-    content = 'Hello Jarvies, what is a 1099?'
-    role = 'user'
-    message = create_message(user_id, conversation_id, content, role)
-    # message_id = message.get().to_dict().get('id')
-    message_id = message
-    print('message_id', message_id)
+# conversation = create_conversation(user_id)
+# # conversation_id = conversation.get().to_dict().get('id')
+# conversation_id = conversation
+# print('conversation_id', conversation_id)
 
-create_user_conversation_message()
+# content = 'Hello Jarvies, what is a 1099?'
+# role = 'user'
+# message = create_message(user_id, conversation_id, content, role)
+# # message_id = message.get().to_dict().get('id')
+# message_id = message
+# print('message_id', message_id)
+
+user_id = 'b51ccb22-ed74-4bd2-8'
+email = 'hector@gmail.com'
+user = update_user(user_id, email)
+print('updated_user', user)
+
+
+
