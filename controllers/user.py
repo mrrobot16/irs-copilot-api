ae3ca8d0feefe544b539cc97bd78c8cf196ec877
Hector G <hmgarcia06@gmail.com>
2023-10-10 22:38:25 -0400

    Added route for getting all users, get user by id and create new users

--- controllers/user.py
+++ controllers/user.py
@@ -1,17 +1,22 @@
 from flask import Blueprint, jsonify, request
-from services.user import get_all_users, create_user
+from services.user import get_users, new_user, get_user
 
 user_controller = Blueprint('user_controller', __name__)
 
-@user_controller.route('/', methods=['GET'])
+@user_controller.route('/health', methods=['GET'])
 def health():
     return jsonify({'status': 'ok'})
 
-@user_controller.route('/get_all', methods=['GET'])
+@user_controller.route('/', methods=['GET'])
 def get_all():
-    return jsonify({'status': 200, 'data': get_all_users()})
+    return jsonify({'status': 200, 'data': get_users()})
 
-@user_controller.route('/create', methods=['POST'])
-def create():
-    # email = request.get_json()['email']
-    return jsonify({'status': 200, 'data': create_user()})
+@user_controller.route('/new', methods=['POST'])
+def new():
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    return jsonify({'status': 200, 'data': new_user(email, password)})
+
+@user_controller.route('/<id>', methods=['GET'])
+def get(id):
+    return jsonify({'status': 200, 'data': get_user(id)})
