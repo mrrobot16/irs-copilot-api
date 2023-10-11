5c1d98a593e6b35359715df3924026c5bab652bf
Hector G <hmgarcia06@gmail.com>
2023-10-10 22:35:34 -0400

    Moved convert doc ref to object to a firebase utils file and added password hashing util

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,40 +1,21 @@
 import uuid
 from datetime import datetime
-from db.firebase import firestore
-
+import bcrypt
 
 # Generate a unique ID
 def generate_unique_id(size=20):
-    return str(uuid.uuid4())[size]
+    return str(uuid.uuid4())[:size]
 
 # Set current timestamp
 def generate_timestamp():
     return datetime.utcnow()
 
-def convert_doc_refs(obj):
-    count = 0
-    """
-    Recursively convert DocumentReference objects in a nested data structure to dictionaries.
-    """
-    if isinstance(obj, firestore.DocumentReference):
-        print('count', count)
-        count += 1
-        # Fetch the actual document data if you want
-        # Be aware of read costs and handle errors appropriately
-        doc = obj.get()
-        if doc.exists:
-            doc_data = doc.to_dict()
-            # Here, we apply convert_doc_refs recursively on the fetched data
-            return convert_doc_refs(doc_data)
-        else:
-            return None  # Handle non-existing documents as you see fit
-        print('count 2', count)
-    elif isinstance(obj, list):
-        # Handle lists by applying the function recursively to each element
-        return [convert_doc_refs(item) for item in obj]
-    elif isinstance(obj, dict):
-        # Handle dictionaries by applying the function recursively to each value
-        return {key: convert_doc_refs(value) for key, value in obj.items()}
-    else:
-        # For any other type, return the object as is
-        return obj
+# Generate a salt & Hash the password along with the salt
+def hash_password(plain_text_password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(plain_text_password.encode('utf-8'), salt)
+    return hashed_password
+
+# Check the provided password against the hashed one
+def verify_hashed_password(plain_text_password, hashed_password):
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
