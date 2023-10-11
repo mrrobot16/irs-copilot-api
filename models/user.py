ceabc965e79aadb72fd23a4ed0bc53ce02fa2f55
Hector G <hmgarcia06@gmail.com>
2023-10-11 19:05:50 -0400

    Added active property and activate/deactivate methods

--- models/user.py
+++ models/user.py
@@ -8,20 +8,23 @@ class User:
         self.db = db # Reference to the Firestore database
         self.collection_ref = self.db.collection('users') # Reference to the 'users' collection
         self.id = None # string
-        self.created_at = None # timestamp
         self.conversations = None # array of conversation reference
         self.email = None # string
         self.password = None # string
-    
+        self.active = True # boolean
+        self.auth_type = 'email-password' # string ['google, apple, github, etc]
+        self.created_at = None # timestamp
+        self.updated_at = None # timestamp
+
     def get_all(self):
         users = self.collection_ref.stream()
 
         # Convert each DocumentSnapshot to a dictionary and add it to a list
         user_list = [user.to_dict() for user in users]
         return user_list
-    
+
     def get(self, id):
-        # Suppress "Prefer using the 'filter' keyword argument instead." warning. 
+        # Suppress "Prefer using the 'filter' keyword argument instead." warning.
         # This happens when using collection_ref.where()
         warnings.filterwarnings("ignore", category=UserWarning, message="Detected filter using positional arguments. Prefer using the 'filter' keyword argument instead.")
 
@@ -30,38 +33,40 @@ class User:
 
         # Users is an iterator of DocumentSnapshot
         # Converting DocumentSnapshot to a dictionary
-        
+
         user_list = [user.to_dict() for user in users]
-        # NOTE: collection_ref.where returns an array of documents. 
+        # NOTE: collection_ref.where returns an array of documents.
         # We only want the first document in the array.
         return user_list[0]
-    
+
     def new(self, email = None, password = None, conversations = []):
         self.id = generate_unique_id()  # Generate a unique 20-character ID
-        print(self.id)
-        self.created_at = generate_timestamp()  # Get current timestamp
         self.email = email
         self.password = hash_password(email) # NOTE: This is a placeholder for now.
         self.conversations = conversations  # Initialize with an empty array of conversation references
+        self.created_at = generate_timestamp()  # Get current timestamp
+        self.updated_at = self.created_at
 
         user = {
             'id': self.id,
-            'created_at': self.created_at,
             'email': self.email,
+            'password': str(self.password),
             'conversations': self.conversations,
-            'password': str(self.password)
+            'active': self.active,
+            'auth_method': self.auth_type,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
-
         # Add the user to the 'users' collection
         user_ref = self.collection_ref.document(self.id)
         user_ref.set(user)
         return user # return user object versus firebase user reference to reduce the amount of reads/writes.
-    
+
     def update(self, id, email, password = None):
         self.id = id
         self.email = email
-        self.updated_at = generate_timestamp()
         # self.password = hash_password(password) if password else hash_password(self.email)
+        self.updated_at = generate_timestamp()
         user = {
             'id': self.id,
             'email': self.email,
@@ -73,8 +78,34 @@ class User:
         users_ref.set(user, merge=True)
         return user
 
+    def activate(self, id):
+        self.id = id
+        self.active = True
+
+        user = {
+            'id': self.id,
+            'active': self.active
+        }
+        users_ref = self.collection_ref.document(self.id)
+        users_ref.set(user, merge=True)
+        return user
+
+    def deactivate(self, id):
+        self.id = id
+        self.active = False
+
+        user = {
+            'id': self.id,
+            'active': self.active
+        }
+        # Here, you can use the set method with merge=True to update or create if the document doesn't exist
+        users_ref = self.collection_ref.document(self.id)
+        users_ref.set(user, merge=True)
+        return user
+
     def delete(self, id):
         self.id = id
+
         user = {
             'id': self.id
         }
