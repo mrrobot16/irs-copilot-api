88b2314e398643988cb1a0d81b78ede69dde39ba
Hector G <hmgarcia06@gmail.com>
2023-10-11 21:56:21 -0400

    Implemented service methods for controller

--- services/conversation.py
+++ services/conversation.py
@@ -4,25 +4,22 @@ from utils.firebase import convert_doc_refs
 
 conversation = Conversation(firestore_db)
 
-# def get_conversations():
-#     conversations = Conversation(firestore_db)
-#     return conversations.get_all()
+def get_conversations():
+    conversations = Conversation(firestore_db)
+    return conversations.get_all()
 
 def new_conversation(user_id, message):
     return conversation.new(user_id, message)
 
-def add_message(user_id, conversation_id, message):
-    return conversation.add_message(conversation_id, message)
+def get_conversation(id):
+    conversation = Conversation(firestore_db)
+    return conversation.get(id)
 
-# def get_coversation(id):
-#     conversation = Conversation(firestore_db)
-#     return conversation.get(id)
+def update_conversation(conversation_id, name):
+    return conversation.update(conversation_id, name)
 
-# def update_conversation(id, email, password = None):
-#     return conversation.update(id, email, password)
+def delete_conversation(id):
+    return conversation.delete(id)
 
-# def delete_conversation(id):
-#     return conversation.delete(id)
-
-# def deactivate_conversation(id):
-#     return conversation.deactivate(id)
+def deactivate_conversation(id):
+    return conversation.deactivate(id)
