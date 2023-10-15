9b4a2314edebf9c5ebd7f2a1b8b20ac8713fbefe
Hector G <hmgarcia06@gmail.com>
2023-10-15 17:35:40 -0400

    Added CORS for localhost:3000

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 import openai
+from flask_cors import CORS
 
 from routes import register
 
 def create_app(config_name='config.py'):
     app = Flask(__name__)
     app.config.from_pyfile(config_name)
+    CORS(app, origins=["http://localhost:3000"])
     return register(app)
 
 
