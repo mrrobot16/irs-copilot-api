3b8deb5ac50dec3a237e09578fc42115108055cc
Hector G <hmgarcia06@gmail.com>
2023-10-15 01:34:38 -0400

    Added test_app to check health of api

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,6 +1,3 @@
 def test_health(client):
     response = client.get('/')
-    print('test_health(client): response', response)
-    # data = json.loads(response.data.decode())
-    # assert response.status_code == 200
-    # assert data['status'] == 'ok'
+    assert response.status_code == 200
