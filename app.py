b5d7355d07a499f7808fdc85f06b8cf7744c1b10
Hector G <hmgarcia06@gmail.com>
2023-10-14 20:35:49 -0400

    moved endpoint code to routes directory

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 from flask import Flask, jsonify
 import openai
 
-from controllers.openai import openai_controller
-from controllers.firebase import firebase_controller
-from controllers.user import user_controller
-from controllers.conversation import conversation_controller
+from routes import register
 
 def create_app(config_name='config.py'):
     app = Flask(__name__)
     app.config.from_pyfile(config_name)
+    return register(app)
 
-    openai_api_key = app.config['OPENAI_API_KEY']
-    openai.api_key = openai_api_key
-
-    app.register_blueprint(openai_controller, url_prefix='/openai')
-    app.register_blueprint(firebase_controller, url_prefix='/firebase')
-    app.register_blueprint(user_controller, url_prefix='/users')
-    app.register_blueprint(conversation_controller, url_prefix='/conversations')
-
-    @app.route('/')
-    def health():
-        return jsonify({'status': 'ok'})
-
-    return app
 
 app = create_app()
 
