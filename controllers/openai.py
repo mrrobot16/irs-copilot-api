f4856b8d1d79cf7433776c4e89e1b6e94fc90fbb
Hector G <hmgarcia06@gmail.com>
2023-10-14 20:43:37 -0400

    Implemented OpenAI in openai services for it to me implemented in openai controller

--- controllers/openai.py
+++ controllers/openai.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
-from services.openai import gpt3_completion, gpt4_completion
+# from services.openai import gpt3_completion, gpt4_completion
+from services.openai import chat_completion
 
 # # Create a blueprint instance
 openai_controller = Blueprint('openai_controller', __name__)
@@ -9,21 +10,8 @@ openai_controller = Blueprint('openai_controller', __name__)
 def health():
     return jsonify({'status': 'ok'})
 
-@openai_controller.route('/chat', methods=['POST'])
+@openai_controller.route('/chat-completion', methods=['POST'])
 def chat():
     prompt = request.get_json()['prompt']
-    response = gpt3_completion(prompt)
-    return jsonify({'response': response})
-
-@openai_controller.route('/chat-gpt4', methods=['POST'])
-def chatgpt4():
-    prompt = request.get_json()['prompt']
-    response = gpt4_completion(prompt)
-    return jsonify({'response': response})
-
-@openai_controller.route('/conversation', methods=['POST'])
-def conversation():
-    prompt = request.get_json()['prompt']
-    response = gpt3_completion(prompt)
-    # response = gpt4_completion(prompt)
+    response = chat_completion(prompt)
     return jsonify({'response': response})
