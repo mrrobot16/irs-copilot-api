b2bf26848e096bf1b3adb2036f4acffc3730b9bd
Hector G <hmgarcia06@gmail.com>
2023-10-14 22:08:01 -0400

    Added constants for openAI error message endpoint

--- constants/openai.py
+++ constants/openai.py
@@ -16,3 +16,8 @@ OPENAI_USER_PROMPT = {
 OPENAI_ENGINE = 'gpt-3.5-turbo-16k'
 OPENAI_TEMPERATURE = 0.5
 OPENAI_MAX_TOKENS = 1000
+
+OPENAI_CHAT_COMPLETION_ENDPOINT_ERROR  = { 
+    'message': "Missing prompt and engine in the request",
+    'status_code': 400
+}
