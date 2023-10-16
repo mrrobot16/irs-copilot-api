d1a386dc74b6a4de47239880cbd5275434ca1cc8
Hector G <hmgarcia06@gmail.com>
2023-10-16 17:39:14 -0400

    Print cred

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,13 +4,13 @@ from controllers.openai import openai_controller
 from controllers.user import user_controller
 from controllers.conversation import conversation_controller
 from flask import jsonify
-
+from config import FIREBASE_CREDENTIALS
 allowed_origins = ['https://irs-copilot.vercel.app/', 'https://irs-copilot.vercel.app', 'http://localhost:3000']
 
 def register(app):
     @app.route('/')
     def health():
-        return jsonify({'status': 'ok'})
+        return jsonify({'status': 'ok', 'FIREBASE_CREDENTIALS': FIREBASE_CREDENTIALS})
 
     app.register_blueprint(openai_controller, url_prefix='/openai')
     app.register_blueprint(user_controller, url_prefix='/users')
