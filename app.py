b9b25ca827d0710fb2c53d142f032aa22266bd31
Hector G <hmgarcia06@gmail.com>
2023-10-16 18:42:45 -0400

    testing printing var len

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from flask_cors import CORS
 
 from routes import register, allowed_origins
 
-from config import FIREBASE_CREDENTIALS
-print("len(FIREBASE_CREDENTIALS['private_key'])", len(FIREBASE_CREDENTIALS['private_key']))
-
 def create_app(config_name='config.py'):
     app = Flask(__name__)
     app.config.from_pyfile(config_name)
