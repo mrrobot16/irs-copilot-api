2aeaf19f9ff2c2370264dbe1627b1584e7139c97
Hector G <hmgarcia06@gmail.com>
2023-10-16 19:48:08 -0400

    Uncommented imports of  controllers using firebase

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,8 @@
 # register.py
 
 from controllers.openai import openai_controller
-# from controllers.user import user_controller
-# from controllers.conversation import conversation_controller
+from controllers.user import user_controller
+from controllers.conversation import conversation_controller
 from flask import jsonify
 
 allowed_origins = ['https://irs-copilot.vercel.app/', 'https://irs-copilot.vercel.app', 'http://localhost:3000']
