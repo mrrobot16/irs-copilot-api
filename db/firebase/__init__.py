91140761cf6959c1020e4e31937227d7ceb6a1e3
Hector G <hmgarcia06@gmail.com>
2023-10-16 15:01:55 -0400

    Removed prints in db/firebase

--- db/firebase/__init__.py
+++ db/firebase/__init__.py
@@ -5,9 +5,7 @@ from config import FIREBASE_CREDENTIALS
 
 # Initialize Firebase Admin SDK
 # credentials = credentials.Certificate("./credentials/firebase/irscopilot_dev-72ar9-0b73aebc83.json")
-print('FIREBASE', FIREBASE_CREDENTIALS)
 credentials = credentials.Certificate(FIREBASE_CREDENTIALS)
-print('credentials', credentials)
 firebase_admin.initialize_app(credentials)
 
 client = firestore.client()
