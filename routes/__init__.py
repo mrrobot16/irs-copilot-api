b9fab5f2eb82d35913e1791725463a78fafb7369
Hector G <hmgarcia06@gmail.com>
2023-10-16 20:15:11 -0400

    Added runtime.txt file to specify version of Python

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,12 +4,10 @@ from controllers.openai import openai_controller
 from controllers.user import user_controller
 from controllers.conversation import conversation_controller
 from flask import jsonify
-from config import FIREBASE_CREDENTIALS
 
 allowed_origins = ['https://irs-copilot.vercel.app/', 'https://irs-copilot.vercel.app', 'http://localhost:3000']
 
 def register(app):
-    print("len(FIREBASE_CREDENTIALS)", len(FIREBASE_CREDENTIALS['private_key']))
     @app.route('/')
     def health():
         return jsonify({'status': 'ok'})
