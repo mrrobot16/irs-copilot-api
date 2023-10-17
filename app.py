dc79c6189c6af0a52fee65d1a45f167c07241075
Hector G <hmgarcia06@gmail.com>
2023-10-16 23:23:03 -0400

    Temp solution

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from routes import register, allowed_origins
 from utils import log_firebase_credentials
 
 def create_app(config_name='config.py'):
-    log_firebase_credentials()
+    # log_firebase_credentials()
     app = Flask(__name__)
     app.config.from_pyfile(config_name)
     CORS(app, origins = allowed_origins)
--- db/firebase/__init__.py
+++ db/firebase/__init__.py
@@ -4,9 +4,13 @@ from datetime import datetime
 from config import FIREBASE_CREDENTIALS
 
 # Initialize Firebase Admin SDK
+
+# Mock credentials
+do_what_ever_credentials = credentials.Certificate('./credentials_example/firebase/do-what-ever-firebase-adminsdk-64t1o-b5724b437b.json')
+firebase_admin.initialize_app(do_what_ever_credentials)
 # credentials = credentials.Certificate("./credentials/firebase/irscopilot_dev-72ar9-0b73aebc83.json")
-credentials = credentials.Certificate(FIREBASE_CREDENTIALS)
-firebase_admin.initialize_app(credentials)
+# credentials = credentials.Certificate(FIREBASE_CREDENTIALS)
+# firebase_admin.initialize_app(credentials)
 
 client = firestore.client()
 
