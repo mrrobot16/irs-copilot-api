418676b83a600ca08273fe40fa08f74e45d308ec
Hector G <hmgarcia06@gmail.com>
2023-10-17 02:31:48 -0400

    In the name of the lord

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,8 @@
 # register.py
 
 from controllers.openai import openai_controller
-# from controllers.user import user_controller
-# from controllers.conversation import conversation_controller
+from controllers.user import user_controller
+from controllers.conversation import conversation_controller
 from flask import jsonify
 
 allowed_origins = ['https://irs-copilot.vercel.app/', 'https://irs-copilot.vercel.app', 'http://localhost:3000']
@@ -13,7 +13,7 @@ def register(app):
         return jsonify({'status': 'ok'})
 
     app.register_blueprint(openai_controller, url_prefix='/openai')
-    # app.register_blueprint(user_controller, url_prefix='/users')
-    # app.register_blueprint(conversation_controller, url_prefix='/conversations')
+    app.register_blueprint(user_controller, url_prefix='/users')
+    app.register_blueprint(conversation_controller, url_prefix='/conversations')
 
     return app
