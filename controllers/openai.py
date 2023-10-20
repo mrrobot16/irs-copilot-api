cf22b0dc08670ea2b4a52f6798e14ea394eb2839
Hector G <hmgarcia06@gmail.com>
2023-10-19 23:02:37 -0400

    Implemented endpoint to check for OpenAI API status

--- controllers/openai.py
+++ controllers/openai.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from services.openai import chat_completion
 from constants.openai import OPENAI_ENGINE, OPENAI_CHAT_COMPLETION_ENDPOINT_ERROR
+from config import OPENAI_API_KEY_DEV
+import requests
 
 # # Create a blueprint instance
 openai_controller = Blueprint('openai_controller', __name__)
@@ -21,3 +23,17 @@ def chat():
     else:
         response = chat_completion(prompt, engine)
         return jsonify({'response': response})
+
+@openai_controller.route('/status', methods=['GET'])
+def check_openai_status():
+
+    headers = {
+        'Authorization': f'Bearer {OPENAI_API_KEY_DEV}',
+    }
+
+    response = requests.get('https://api.openai.com/v1/engines', headers=headers)
+    
+    if response.status_code == 200:
+        return jsonify({"status": "success", "message": "OpenAI API is operational."}), 200
+    else:
+        return jsonify({"status": "failure", "message": f"Issue with OpenAI API. Status code: {response.status_code}"}), 500
