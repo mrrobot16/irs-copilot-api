b5c58a3b9b0daf8f2f196ee0615dff4acf9e6050
Hector G <hmgarcia06@gmail.com>
2023-10-26 18:47:17 -0400

    Added try/catch to sentry init function

--- utils/sentry.py
+++ utils/sentry.py
@@ -6,10 +6,16 @@ traces_sample_rate = 1.0
 profiles_sample_rate=1.0
 
 def sentry_init():
-    sentry_sdk.init(
-        dsn=dsn,
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        traces_sample_rate=traces_sample_rate,
-        profiles_sample_rate=profiles_sample_rate
-    )
+    try:
+        sentry_sdk.init(
+            dsn=dsn,
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for performance monitoring.
+            traces_sample_rate=traces_sample_rate,
+            profiles_sample_rate=profiles_sample_rate
+        )
+    except Exception as error:
+        print('error in utils/sentry.py/sentry_init()', error)
+        sentry_sdk.capture_exception(error)
+        sentry_sdk.flush()
+        raise error
